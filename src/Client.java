eba93333df15c62e3f9075c02e88d7866d44a3fb
Steven Burris <stevenburris.jr@gmail.com>
2016-09-12 13:23:56 -0400

    Created the MedicalCondition Class

--- src/Client.java
+++
@@ -1,5 +0,0 @@
-/**
- * Created by stevenburris on 9/12/16.
- */
-public class Client {
-}
---
+++ src/MedicalCondition.java
@@ -0,0 +1,13 @@
+/**
+ * Created by stevenburris on 9/12/16.
+ */
+public class MedicalCondition {
+
+    String nameofCondition;
+    boolean critical;
+    boolean treatable;
+    String timeWith;
+    int daysInBed;
+
+
+}
