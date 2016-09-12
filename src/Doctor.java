88980d22ad4f628eb31b776a687578b48cbd4816
Steven Burris <stevenburris.jr@gmail.com>
2016-09-12 17:32:13 -0400

    created classes

--- src/Doctor.java
+++ src/Doctor.java
@@ -1,9 +1,10 @@
 /**
  * Created by stevenburris on 9/12/16.
- * Name, age, and years of experience of doctors
+ * Name, age, years of experience of doctors, and schools attended
  */
 public class Doctor {
     String name;
     int age;
     int yearsWorked;
+    String[] education = new String[3];
 }
---
+++ src/Patients.java
@@ -0,0 +1,12 @@
+/**
+ * Created by stevenburris on 9/12/16.
+ */
+public class Patients {
+    String name;
+    int age;
+    String dateOfBirth;
+    String checkIn;
+    String checkOut;
+    String sickWith;
+
+}
