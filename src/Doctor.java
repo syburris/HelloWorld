09a4df34c1638296c9a718e8671bf4029a0a0b66
Steven Burris <stevenburris.jr@gmail.com>
2016-09-13 13:22:48 -0400

    created classes

--- src/Doctor.java
+++ src/Doctor.java
@@ -7,4 +7,30 @@ public class Doctor {
     int age;
     int yearsWorked;
     String[] education = new String[3];
+
+    String getName() {
+        return name;
+    }
+
+    void setName(String newName){
+        name = newName;
+    }
+
+    int getAge() {
+        return age;
+    }
+
+    void setAge(int newAge) {
+        age = newAge;
+    }
+
+    int getYearsWorked(){
+        return yearsWorked;
+    }
+
+    void setYearsWorked(int newYearsWorked){
+        yearsWorked = newYearsWorked;
+    }
+
+
 }
