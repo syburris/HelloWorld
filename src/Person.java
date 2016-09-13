3aab004cf0bdc279c9fd6f8acbb20eaebaea8ff3
Steven Burris <stevenburris.jr@gmail.com>
2016-09-13 10:35:41 -0400

    add methods to person class

--- src/Person.java
+++ src/Person.java
@@ -5,4 +5,40 @@ public class Person {
     String name;
     int age;
     boolean isAlive;
+
+    String getName(){
+        return name;
+    }
+
+    void setName(String newName) {
+        if (newName.contains(" ")) {
+            name = newName;
+        }
+    }
+
+    int getAge() {
+        return age;
+    }
+
+    void setAge(int newAge) {
+        if (newAge >= 18) {
+            age = newAge;
+        }
+    }
+
+    boolean getIsAlive(){
+        return isAlive;
+    }
+
+    void setIsAlive(boolean newIsAlive) {
+        if(age > 100) {
+            isAlive = false;
+        }
+        else {
+            isAlive = newIsAlive;
+        }
+    }
+
+
+
 }
