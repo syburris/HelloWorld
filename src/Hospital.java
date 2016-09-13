6fcd51d741e0aae89711a97fad96a49a0cdbcfb5
Steven Burris <stevenburris.jr@gmail.com>
2016-09-13 16:56:19 -0400

    created classes

--- src/Hospital.java
+++ src/Hospital.java
@@ -9,4 +9,25 @@ public class Hospital {
     Address address;
     String[] doctors = new String[5];
 
+
+    String getNameofHospital(){
+        return nameofHospital;
+    }
+
+    int getYearsOpen(){
+        return yearsOpen;
+    }
+
+    boolean getHasER(){
+        return hasER;
+    }
+
+    Address getAddress(){
+        return address;
+    }
+
+    String getAddress2(){
+        return address.street + System.lineSeparator() + address.city +", "+ address.state + " " + address.zipCode + System.lineSeparator() + address.country;
+    }
 }
+
