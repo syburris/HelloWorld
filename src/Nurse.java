ffdddbbbc514bc304b750e6a763b8c7857f8d7be
Steven Burris <stevenburris.jr@gmail.com>
2016-09-13 20:47:01 -0400

    created classes

--- src/Nurse.java
+++ src/Nurse.java
@@ -10,4 +10,61 @@ public class Nurse {
     int yearsExp;
     String currentHospital;
 
+    String getName(){
+        return name;
+    }
+
+    void setName(String newName){
+        name = newName;
+    }
+
+    int getAge(){
+        return age;
+    }
+
+    void setAge(int newAge){
+        age = newAge;
+    }
+
+    String getSpecialization1(){
+        return specialization[0];
+    }
+
+    void setSpecialization1(String newSpecialization1){
+        specialization[0] = newSpecialization1;
+    }
+
+    String getSpecialization2(){
+        return specialization[1];
+    }
+
+    void setSpecialization2(String newSpecialization2){
+        specialization[1] = newSpecialization2;
+    }
+
+    String getSpecialization3(){
+        return specialization[2];
+    }
+
+    void setSpecialization3(String newSpecialization3){
+        specialization[2] = newSpecialization3;
+    }
+
+    int getYearsExp(){
+        return yearsExp;
+    }
+
+    void setYearsExp(int newYearsExp){
+        yearsExp = newYearsExp;
+    }
+
+    String getCurrentHospital(){
+        return currentHospital;
+    }
+
+    void setCurrentHospital(String newHospital){
+        currentHospital = newHospital;
+    }
+
+
 }
