903d775b97bdbd62a1fed6f11525982cb44e8f77
Steven Burris <stevenburris.jr@gmail.com>
2016-09-15 10:53:48 -0400

    Created different types of loops in main

--- src/Main.java
+++ src/Main.java
@@ -8,21 +8,36 @@ public class Main {
         names[1] = "bob";
         names[2] = "charlie";
 
-        //while loop
-        int index = 0;
-        while(index < names.length){
-            System.out.println(names[index]);
-            index++;
-        }
+//        //while loop
+//        int index = 0;
+//        while(index < names.length){
+//            System.out.println(names[index]);
+//            index++;
+//        }
+//
+//        //traditional for loop
+//        for (int i = 0; i < names.length; i++){
+//            System.out.println(names[i]);
+//        }
+//        //new for loop
+//        for(String name : names){
+//            System.out.println(name);
+//        }
 
-        //traditional for loop
-        for (int i = 0; i < names.length; i++){
-            System.out.println(names[i]);
-        }
-        //new for loop
-        for(String name : names){
-            System.out.println(name);
-        }
+        // int -> Integer
+        // long -> Long
+        //boolean -> Boolean
+        // ^ above shows unboxed -> boxed
+
+        boolean b = Boolean.valueOf("1");
+        System.out.println(b);
+
+        int i = Integer.valueOf("123");
+        System.out.println(i);
+
+
+
+
     }
 }
 
