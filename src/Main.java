2fc72e5f3a37c18d7da7ece2320fe652625767f1
Steven Burris <stevenburris.jr@gmail.com>
2016-09-15 09:59:20 -0400

    loops

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+/**
+ * Created by stevenburris on 9/14/16.
+ */
+public class Main {
+    public static void main(String[] args){
+        String[] names = new String[3];
+        names[0] = "alice";
+        names[1] = "bob";
+        names[2] = "charlie";
+
+        //while loop
+        int index = 0;
+        while(index < names.length){
+            System.out.println(names[index]);
+            index++;
+        }
+
+        //traditional for loop
+
+    }
+}
+
+
