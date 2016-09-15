cff43a67c025d0c1f771ce8a7fd3f2eed3d0b4ab
Steven Burris <stevenburris.jr@gmail.com>
2016-09-15 11:19:53 -0400

    Created different types of loops in main

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import java.util.*;
+import java.util.function.BooleanSupplier;
+
 /**
  * Created by stevenburris on 9/14/16.
  */
@@ -29,15 +32,32 @@ public class Main {
         //boolean -> Boolean
         // ^ above shows unboxed -> boxed
 
-        boolean b = Boolean.valueOf("1");
+        boolean b = Boolean.valueOf(true);
         System.out.println(b);
 
-        int i = Integer.valueOf("123");
-        System.out.println(i);
+
+        //ArrayLists
+        ArrayList<String> names2 = new ArrayList();
+        names2.add("alice");
+        names2.add("bob");
+        names2.add("charlie");
+        names2.remove(1);
+        System.out.println(names2);
+
+        //HashMaps
+        HashMap<String, Object> person = new HashMap();
+        person.put("name", "alice");
+        person.put("age", 30);
+        person.put("location", "Charleston");
+        person.put("name", "bob");
+        person.remove("age");
+        person.put("names", names2);
+        System.out.println(person.get("name"));
 
 
 
-
+        String alice = names2.get(0);
+        String bob = (String) person.get("name");
     }
 }
 
