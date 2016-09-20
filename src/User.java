21a02ab5ffa791c0f8c3b87496493851b0fd0fc6
Steven Burris <stevenburris.jr@gmail.com>
2016-09-20 10:37:07 -0400

    email program

---
+++ src/User.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+/**
+ * Created by stevenburris on 9/20/16.
+ */
+public class User {
+    String name;
+    String password;
+    ArrayList<Email> emails = new ArrayList<>();
+
+}
